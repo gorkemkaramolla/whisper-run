b528831e677ee9901207c24768c416e2e7f4af58
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2024-07-19 10:06:38 +0300

    oyannote_onnx exported

--- src/whisper_run/__init__.py
+++ src/whisper_run/__init__.py
@@ -5,3 +5,4 @@ from .audio_converter import AudioConverter
 from .audio_processor import AudioProcessor
 from .config import whisper_models
 from .arg_parser import parse_arguments
+from .pyannote_onnx import *
